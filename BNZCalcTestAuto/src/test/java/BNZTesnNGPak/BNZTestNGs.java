5f5fcf967aecdbb2de2c5dced7e5d20ded2bf6df
ShyamsundarPeriyasamy <shyam.tandem@gmail.com>
2018-02-02 19:08:46 +1300

    Initial commit

---
+++ BNZCalcTestAuto/src/test/java/BNZTesnNGPak/BNZTestNGs.java
@@ -0,0 +1,15 @@
+package BNZTesnNGPak;
+
+import org.testng.annotations.Test;
+
+import BNZCommonMethodsPak.BNZCalcCommonMethodsC;
+
+public class BNZTestNGs {
+	
+	@Test
+	
+	public void testMainPage() {
+		BNZCalcCommonMethodsC.TestBNZ();
+	}
+
+}
---
+++ BNZCalcTestAuto/src/test/java/WestPacCommonMethodsPak/WestPakCommonMethodsC.java
@@ -0,0 +1,5 @@
+package WestPacCommonMethodsPak;
+
+public class WestPakCommonMethodsC {
+
+}
